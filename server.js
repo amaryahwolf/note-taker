dda38b7c717748899d1258089a1095c5cefeacb6
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-01-05 17:33:24 -0800

    add GET functionality for public html files

---
+++ server.js
@@ -0,0 +1,28 @@
+// Require libraries and files
+const express = require('express');
+const path = require('path');
+const api = require('./routes/notes');
+
+// Initialize server app and server it will run on
+const PORT = process.env.port ||  3001;
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// GET route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET wildcard route
+app.get('*', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+ );
+
+// Event listener
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
